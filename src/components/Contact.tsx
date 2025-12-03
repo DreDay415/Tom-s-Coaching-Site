a766fbc8a8de03ea6dcf1ab5e36a6cb4fdb459df
DreDay415 <DreDay415@users.noreply.github.com>
2025-12-03 15:30:26 -0800

    Refresh about bio and footer layout

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -254,7 +254,7 @@ const Contact: React.FC = () => {
                   <svg className="w-5 h-5 text-gold-400 mr-3" fill="currentColor" viewBox="0 0 20 20">
                     <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                   </svg>
-                  <span className="text-gold-200">30-minute free consultation</span>
+                  <span className="text-gold-200">30-minute consultation</span>
                 </div>
                 <div className="flex items-center">
                   <svg className="w-5 h-5 text-gold-400 mr-3" fill="currentColor" viewBox="0 0 20 20">
